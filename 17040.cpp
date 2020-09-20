26dda3841476412592dc9857259c48f6cd1daca0
Justiceserv <guhyun.chung@stpaulseoul.org>
2020-09-20 18:19:42 +0900

    Create 17040.cpp

---
+++ 17040.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+int main(void)
+{
+  int N, M;
+  int A[151], B[151], G[101];
+  cin >> N >> M;
+  for (int i=0; i<M; i++) { 
+    cin >> A[i] >> B[i];
+    if (A[i] > B[i]) 
+        swap (A[i], B[i]);
+  }
+  for (int i=1; i<=N; i++) {
+    int g;
+    for (g = 1; g <= 4; g++) {
+      bool ok = true;
+      for (int j=0; j<M; j++) 
+	if (B[j] == i && G[A[j]] == g) 
+        ok = false;
+      if (ok) break;
+    }
+    G[i] = g;
+    cout << g;
+  }
+  cout << "\n";
+  return 0;
+}
